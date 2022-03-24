ea15c928c3a3385bd3e12f4ba531ce792ae9b751
Vaibhav9526 <81508584+Vaibhav9526@users.noreply.github.com>
2022-03-24 17:15:48 +0530

    Add files via upload

---
+++ Average-finder/average.js
@@ -0,0 +1,16 @@
+//FOR 2 NUMBERS 
+
+function TwoAvg(){
+    var a = document.getElementById("txt-o");
+    var b = document.getElementById("txt-t");
+    var y = (a)+(b);
+    var z = (y/2);
+    document.getElementById("Demo").innerHTML = "The Average Of " + a + " and " + b + " is " + y;
+    
+    // IF ELSE FOR A = EMPTY
+    
+//     if(a==""){
+//         document.getElementById("Demo").innerHTML = "Please Enter A Number!";
+//     }else{
+// }
+}
